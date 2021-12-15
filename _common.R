4056b8da306c25e47031f7ff9265d66abe5d64a4
Lauren O'Brien <obrienl@landcareresearch.co.nz>
2021-12-16 11:55:28 +1300

    fix captioning and r chunk options
    options centralised in common.R
    hashtags defining internal links in captions need to be \\ escaped for PDF build. This has no effect on BS4 format.

--- _common.R
+++ _common.R
@@ -4,5 +4,10 @@ options(width = 60)
 # example chunk options set globally
 knitr::opts_chunk$set(
   comment = "#>",
-  collapse = TRUE
+  collapse = TRUE,
+  out.width = '85%',
+  fig.align ='center',
+  echo = FALSE,
+  fig.show = 'hold',
+  results = 'hold'
   )
